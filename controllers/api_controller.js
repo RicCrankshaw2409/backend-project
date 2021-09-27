db547204efdfa6b802a76a64a3c2ca6945609351
Richard Crankshaw <ric_24@hotmail.co.uk>
2021-09-27 12:18:55 +0100

    End-points added to getEndpoints

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -12,6 +12,14 @@ exports.getEndPoints = (req, res, next) => {
           PATCH: "/api/reviews/review_id",
           POST: "/api/reviews/review_id/comments",
         },
+        comments: {
+          DELETE: "/api/comments/:comment_id",
+          PATCH: "/api/comments/:comment_id",
+        },
+        users: {
+          GET: "/api/users",
+          GET: "/api/users/:username",
+        },
       },
     });
   } catch (err) {
