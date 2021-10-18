5285d75e453243a430dc0dd44b90cc806cb908c0
Richard Crankshaw <ric_24@hotmail.co.uk>
2021-10-18 16:17:08 +0100

    Package.json updated

--- routers/api_router.js
+++ routers/api_router.js
@@ -4,9 +4,10 @@ const reviewRouter = require("./review_router");
 const commentRouter = require("./comment_router");
 const userRouter = require("./user.router");
 const app = require("../app");
-const { getEndPoints } = require("../controllers/api_controller");
+const getEndPoints = require("../controllers/api_controller");
 
 apiRouter.route("/").get(getEndPoints);
+
 apiRouter.use("/categories", categoryRouter);
 apiRouter.use("/reviews", reviewRouter);
 apiRouter.use("/comments", commentRouter);
